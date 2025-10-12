e5272ebef4039547dd3a07b6e5b81a905479666b
ZHOU <zhou@MacBook-Air.local>
2025-10-12 15:54:07 +0800

    W04-P2: Create blog_xx table with 3 data, implement route /api/blog_45 to return a json array with 3 data

--- w04_blogs_fullstack_45/server_blogs_45/server.js
+++ w04_blogs_fullstack_45/server_blogs_45/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
-import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+import db from './utils/database.js';
+
+app.use('/api/blog_45', async (req, res, next) => {
+    const results = await db.query(`select * from blog_45`);
+console.log('results', JSON.stringify(results.rows));
+res.json(results.rows);
+})
 
 app.use('/', (req, res, next) => {
-    res.send('周淵凱,213410045');
+        res.send('周淵凱,213410045');
 });
 
 const port = process.env.PORT || 5001;
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
--- w04_blogs_fullstack_45/server_blogs_45/utils/database.js
+++ w04_blogs_fullstack_45/server_blogs_45/utils/database.js
@@ -2,7 +2,7 @@ import pg from 'pg';
 const { Pool } = pg;
 let pool;
 
-if (process.env.DATABASE='LOCAL') {
+if (process.env.DATABASE ==='LOCAL') {
     pool = new Pool({
         host: 'localhost',
         port:'5432',
@@ -11,7 +11,15 @@ if (process.env.DATABASE='LOCAL') {
         database: 'wp1_demo_45'
     });
     console.log('connecting to local database', pool.options.database)
-} else if(process.env.DATABASE='SUPABASE') {
+} else if(process.env.DATABASE === 'SUPABASE') {
+    pool = new Pool({
+        host: '',
+        port:'',
+        user: '',
+        password: '',
+        database: ''
+    });
+    console.log('Connecting Supabase', pool.options.database)
 }   
 
 export default pool;
